81216d8b14b68a95640321d5fd4bcd1ec8ce270c
weiaodi <weiaodi@kuaishou.com>
2025-10-16 10:55:19 +0800

    feat: 初始化项目

---
+++ rsbuild-project/src/index.ts
@@ -0,0 +1,9 @@
+const rootEl = document.querySelector('#root');
+if (rootEl) {
+  rootEl.innerHTML = `
+  <div class="content">
+    <h1>Vanilla Rsbuild</h1>
+    <p>Start building amazing things with Rsbuild.</p>
+  </div>
+`;
+}
